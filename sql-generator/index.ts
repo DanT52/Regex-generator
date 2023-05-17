79c41f460439477fc66da994fad47354a9e8bbad
DanT52 <dantramp52@gmail.com>
2023-05-16 23:04:05 -0700

    projected created worked on ui

---
+++ sql-generator/index.ts
@@ -0,0 +1,8 @@
+const PORT: number  = 8000
+
+import express, {Application, Request, Response} from "express"
+
+import cors from "cors"
+
+
+const API_KEY: string = "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"
---
+++ sql-generator/src/components/MessagesDisplay.tsx
@@ -0,0 +1,20 @@
+import MessageDisplay from "./MessageDisplay";
+
+const MessagesDisplay =() => {
+    return (
+      <div className="messages-display">
+        <MessageDisplay/>
+        <MessageDisplay/>
+        <MessageDisplay/>
+        <MessageDisplay/>
+        <MessageDisplay/>
+        <MessageDisplay/>
+        <MessageDisplay/>
+        <MessageDisplay/>
+        <MessageDisplay/>
+      </div>
+    );
+  }
+  
+  export default MessagesDisplay;
+  
